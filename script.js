aabcfb041c1483232c850e853e57222c198659dc
Obatomi01 <eniolaoba9@gmail.com>
2022-08-19 14:25:07 +0100

    🚴‍♀️ update

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 "use script";
 
 const slides = document.querySelectorAll(".work-slide");
+const works = document.querySelector(".works-slide");
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
 
 let slideIndex = 0;
 const avg = Math.floor(slides.length / 2);
 const angleDetails = {};
-let newAngle;
+let newAngle, maxLimit, minLimit;
 
 const translateImages = function () {
   slides.forEach((el, i) => {
@@ -32,7 +33,7 @@ const goToSlide = function (slide) {
 const moveLeft = function () {
   slideIndex -= 1;
   slides.forEach((el, i) => {
-    if (slideIndex < -2) {
+    if (slideIndex < minLimit) {
       newAngle = `translateX(${angleDetails[i] - 400}%)`;
     } else {
       newAngle = `translateX(${angleDetails[i] + 100}%)`;
@@ -41,7 +42,7 @@ const moveLeft = function () {
     newAngle = Number(el.style.transform.slice(10).slice(1).replace("%)", ""));
     angleDetails[i] = newAngle;
   });
-  if (slideIndex < -2) slideIndex = 2;
+  if (slideIndex < minLimit) slideIndex = maxLimit;
 };
 
 const moveRight = function () {
@@ -49,20 +50,35 @@ const moveRight = function () {
 
   slides.forEach((el, i) => {
     newAngle =
-      slideIndex > 2
+      slideIndex > maxLimit
         ? `translateX(${angleDetails[i] + 400}%)`
         : `translateX(${angleDetails[i] - 100}%)`;
     el.style.transform = newAngle;
     newAngle = Number(el.style.transform.slice(10).slice(1).replace("%)", ""));
     angleDetails[i] = newAngle;
   });
-  if (slideIndex > 2) slideIndex = -2;
+  if (slideIndex > maxLimit) slideIndex = minLimit;
 };
 
-goToSlide(2);
+// goToSlide(2);
 btnLeft.addEventListener("click", moveLeft);
 btnRight.addEventListener("click", moveRight);
 document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") moveRight();
   if (e.key === "ArrowLeft") moveLeft();
 });
+
+function reportWindowSize() {
+  if (window.innerHeight < 500) {
+    goToSlide(0);
+    maxLimit = 4;
+    minLimit = 0;
+  } else {
+    goToSlide(2);
+    maxLimit = 2;
+    minLimit = -2;
+  }
+}
+
+reportWindowSize();
+// window.onresize = reportWindowSize;
